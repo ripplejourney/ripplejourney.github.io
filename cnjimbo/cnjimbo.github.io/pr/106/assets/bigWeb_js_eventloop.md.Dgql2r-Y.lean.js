86197e00cab848a788ff5fa680e19152be05aa30
cnjimbo <cnjimbo@users.noreply.github.com>
2024-06-25 06:24:35 +0000

    ci: deploy preview for cnjimbo/cnjimbo.github.io/pr/106
    Commit created with [EndBug/pages-preview](https://github.com/EndBug/pages-preview)

---
+++ cnjimbo/cnjimbo.github.io/pr/106/assets/bigWeb_js_eventloop.md.Dgql2r-Y.lean.js
@@ -0,0 +1,5 @@
+import{_ as t,o as k,c as p,j as s,I as n,w as l,a as h,a5 as i,D as e}from"./chunks/framework.BuhKPXqt.js";const D=JSON.parse('{"title":"EventLoop","description":"","frontmatter":{"sidebar":{"title":"Event Loop","step":12},"isTimeLine":true,"title":"EventLoop","date":"2020-04-14T00:00:00.000Z","tags":["大前端","javascript"],"categories":["大前端"]},"headers":[],"relativePath":"bigWeb/js/eventloop.md","filePath":"bigWeb/js/eventloop.md","lastUpdated":1671802927000}'),E={name:"bigWeb/js/eventloop.md"},r=i("",47),d=s("p",null," 输出结果",-1),g=s("pre",null,[s("code",null,`  
+  // 1 4 7 5 2 3 6
+  `)],-1),o=i("",2),y=s("p",null," 输出结果",-1),c=s("pre",null,[s("code",null,`  
+  // 1 4 10 5 6 7 2 3 9 8
+  `)],-1),F=i("",38);function C(u,m,B,b,v,T){const a=e("font");return k(),p("div",null,[r,s("details",null,[s("summary",null,[s("mark",null,[n(a,{color:"darkred"},{default:l(()=>[h("点击查看答案")]),_:1})])]),d,g]),o,s("details",null,[s("summary",null,[s("mark",null,[n(a,{color:"darkred"},{default:l(()=>[h("点击查看答案")]),_:1})])]),y,c]),F])}const _=t(E,[["render",C]]);export{D as __pageData,_ as default};
